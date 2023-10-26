9dc8a33ce1980c74ffd6f849fe898d33d234017b
Anon <anonchama2@proton.me>
2023-10-25 19:22:01 -0600

    added gui

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
-console.log("YAMU LOVES");
+var globalData = [];
+const postlist = document.querySelector('#postlist');
 
-$.getJSON("https://anon-chama.github.io/hilos.json")
+
+$.getJSON("./database.json")
 .done(function( data ) {
-   console.log(data)
+    globalData = data;
 });
+
+
+
+$("#boton-buscar").click(function(){
+    let term = $("#term-box").val().toLowerCase();
+    $('#postlist').empty();
+    let coincidences = 0;
+    globalData.forEach(post => {
+        text = post["content"];
+        if(text.toLowerCase().includes(term)){
+            coincidences++;
+            if(coincidences > 9000) return;
+            let div = document.createElement('div');
+            div.classList.add("post-wraper");
+            div.innerHTML = `
+                <span class="post-details">
+                    <span class="name">Anonymous</span>
+                    <span class="timestamp">${post["date"]}</span>
+                    <span class="post-number">No.${post["id"]}</span>
+                    <a class="goto-button" target="_blank" href="https://archive.palanq.win/vt/thread/${post["father"]}/#${post["id"]}">View on archive</a>
+                    
+                    <div class="content">
+                    <img class="image-link" src="https://archive-media.palanq.win/vt/thumb${post["image"]}" onerror="this.style.display='none'">
+                    <div class="text">
+                        ${post["content"].replaceAll("\n", "<br>")}
+                    </div>
+                    </div>
+                </span>
+            `
+            postlist.appendChild(div);
+            postlist.appendChild(document.createElement('br'));
+        }
+    });
+    $("#result").text(`Your query has been found on ${coincidences} posts!`)
+});
+
+
+
